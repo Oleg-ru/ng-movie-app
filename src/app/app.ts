044e9045fffc33d6a3455d8c715fd26698b9334d
oleg <oleg_09-95@mail.ru>
2026-07-29 21:38:47 +0300

    bug: пока не получается загрузить картинки. Либо амазон блочит по стране 403 либо хз

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SERVICE } from './core/core';
 import { AsyncPipe } from '@angular/common';
+import { MovieCard } from './features/movies/components/movie-card/movie-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, MovieCard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
