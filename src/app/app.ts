55ddb1463afd8a143cd823edfc557b5d1ddfa4b6
oleg <oleg_09-95@mail.ru>
2026-08-01 21:08:52 +0300

    feature: добавил шрифты, создал header и начал верстать

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-
-}
+export class App {}
