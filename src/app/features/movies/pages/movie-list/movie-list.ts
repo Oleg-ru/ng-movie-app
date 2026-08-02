bbb1356b07b8632872e295dd8f510d95c72aca26
oleg <oleg_09-95@mail.ru>
2026-08-02 21:07:06 +0300

    feature: исправлен баг, что при изменении типа списка не загружался нужный. Реализовано через ИМПЕРАТИВНЫЙ подход через subscribe

--- src/app/features/movies/pages/movie-list/movie-list.ts
+++ src/app/features/movies/pages/movie-list/movie-list.ts
@@ -1,13 +1,12 @@
 import { Component, computed, inject, signal } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
 import { MovieCard } from '../../components/movie-card/movie-card';
 import { SERVICE } from '../../../../core/core';
 import { ActivatedRoute } from '@angular/router';
-import { MoviesList } from '../../../../core/models/MovieList';
+import { MovieListResponse, MoviesList } from '../../../../core/models/MovieList';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [AsyncPipe, MovieCard],
+  imports: [MovieCard],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.scss',
 })
@@ -15,7 +14,7 @@ export class MovieList {
   private activatedRoute = inject(ActivatedRoute);
   protected category = signal<MoviesList>('popular');
   protected discoverService = inject(SERVICE.discover);
-  protected discovers = this.discoverService.getMovieList('1', this.category());
+  protected discovers = signal<MovieListResponse | null>(null);
 
   protected titleList: Record<Exclude<MoviesList, null>, string> = {
     popular: 'Популярные',
@@ -26,9 +25,26 @@ export class MovieList {
   protected currentTitle = computed(() => {
     const title = this.category();
     return this.titleList[title ?? 'popular'] ?? 'Фильмы';
-  })
+  });
 
   constructor() {
-    this.category.set(this.activatedRoute.snapshot.paramMap.get('category') as MoviesList);
+    //this.category.set(this.activatedRoute.snapshot.paramMap.get('category') as MoviesList);
+
+  }
+
+  loadMovies() {
+    this.discoverService.getMovieList('1', this.category()).subscribe({
+      next: (data) => this.discovers.set(data),
+      error: (err) => console.error(err)
+    });
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params['category']);
+      this.category.set(params['category']);
+      console.log(this.category());
+    });
+    this.loadMovies();
   }
 }
