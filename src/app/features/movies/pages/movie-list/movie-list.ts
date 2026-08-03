cf0850fff5d99011a386319bbe51ed2895addaa6
oleg <oleg_09-95@mail.ru>
2026-08-03 20:11:47 +0300

    feature: компонент списка переписан на реактивный подход

--- src/app/features/movies/pages/movie-list/movie-list.ts
+++ src/app/features/movies/pages/movie-list/movie-list.ts
@@ -1,50 +1,63 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MovieCard } from '../../components/movie-card/movie-card';
 import { SERVICE } from '../../../../core/core';
 import { ActivatedRoute } from '@angular/router';
-import { MovieListResponse, MoviesList } from '../../../../core/models/MovieList';
+import { MoviesList } from '../../../../core/models/MovieList';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [MovieCard],
+  imports: [MovieCard, AsyncPipe],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.scss',
 })
 export class MovieList {
   private activatedRoute = inject(ActivatedRoute);
-  protected category = signal<MoviesList>('popular');
   protected discoverService = inject(SERVICE.discover);
-  protected discovers = signal<MovieListResponse | null>(null);
-
   protected titleList: Record<Exclude<MoviesList, null>, string> = {
     popular: 'Популярные',
     now_playing: 'Сейчас смотрят',
     top_rated: 'Лучшие',
     upcoming: 'Ожидаемые',
   };
-  protected currentTitle = computed(() => {
-    const title = this.category();
-    return this.titleList[title ?? 'popular'] ?? 'Фильмы';
-  });
 
-  constructor() {
-    //this.category.set(this.activatedRoute.snapshot.paramMap.get('category') as MoviesList);
+  //Реактивная
+  protected category$ = this.activatedRoute.params.pipe(
+    map((params) => params['category'] ?? 'popular'),
+  );
+  protected discovers$ = this.category$.pipe(
+    switchMap((category: MoviesList) => {
+      return this.discoverService.getMovieList('1', category);
+    }),
+    catchError((err) => {
+      console.error(err);
+      return of(null);
+    }),
+  );
+  protected currentTitle$ = this.category$.pipe(
+    map((category: MoviesList) => this.titleList[category ?? 'popular'] || 'Фильмы'),
+  );
 
-  }
-
-  loadMovies() {
-    this.discoverService.getMovieList('1', this.category()).subscribe({
-      next: (data) => this.discovers.set(data),
-      error: (err) => console.error(err)
-    });
-  }
-
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
-      console.log(params['category']);
-      this.category.set(params['category']);
-      console.log(this.category());
-    });
-    this.loadMovies();
-  }
+  //Императивная
+  // protected category = signal<MoviesList>('popular');
+  // protected discovers = signal<MovieListResponse | null>(null);
+  // protected currentTitle = computed(() => {
+  //   const title = this.category();
+  //   return this.titleList[title ?? 'popular'] ?? 'Фильмы';
+  // });
+  // loadMovies() {
+  //   this.discoverService.getMovieList('1', this.category()).subscribe({
+  //     next: (data) => this.discovers.set(data),
+  //     error: (err) => console.error(err),
+  //   });
+  // }
+  // ngOnInit() {
+  //   this.activatedRoute.params.subscribe((params) => {
+  //     console.log(params['category']);
+  //     this.category.set(params['category']);
+  //     console.log(this.category());
+  //   });
+  //   this.loadMovies();
+  // }
 }
