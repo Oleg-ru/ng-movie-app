4b342cc076571319d683e2912b0be0be5fb5b013
oleg <oleg_09-95@mail.ru>
2026-08-05 21:25:13 +0300

    feature: сделан переход по ссылкам в movies

--- src/app/core/constants/menu.constants.ts
+++ src/app/core/constants/menu.constants.ts
@@ -11,7 +11,7 @@ export interface SubMenuItem {
 
 export const MENU_ITEMS: MenuItem[] = [
   {
-    id: 'movie',
+    id: 'movies',
     title: 'Фильмы',
     children: [
       {
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,5 +1,6 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, signal, WritableSignal } from '@angular/core';
 import { MENU_ITEMS } from '../../../core/constants/menu.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,32 +11,59 @@ import { MENU_ITEMS } from '../../../core/constants/menu.constants';
 export class Header {
   protected menuList = MENU_ITEMS.map((item) => item.title);
   menuState = new Map<string, WritableSignal<boolean>>();
+  protected router = inject(Router);
 
   constructor() {
     MENU_ITEMS.forEach((id) => this.menuState.set(id.title, signal(false)));
   }
 
   protected subMenu = computed(() => {
-    let activeTitle = ''
+    let activeTitle = '';
     this.menuState.forEach((state, title) => {
       if (state()) {
         activeTitle = title;
       }
     });
 
-    return MENU_ITEMS.find(menu => menu.title === activeTitle)?.children?.map(child => child.label)
+    return MENU_ITEMS.find((menu) => menu.title === activeTitle)?.children?.map(
+      (child) => child.label,
+    );
   });
 
   isOpen(id: string) {
     return this.menuState.get(id)?.();
   }
 
-  isClose(id: string) {
+  isClose() {
     this.menuState.forEach((menu) => menu.set(false));
   }
 
   toggleMenu(id: string) {
     this.menuState.forEach((menu) => menu.set(false));
     this.menuState.get(id)?.set(true);
+  }
+
+  moveTo(itemMenu: string, menu: string) {
+    //const urls = this.router.url.split('/').filter((item) => (item.length > 1 ? item : ''));
+    let parentUrl = '';
+    let childUrl = '';
+
+    MENU_ITEMS.find((menuItem) => {
+      const isValid = menuItem.title === itemMenu;
+      if (isValid) {
+        parentUrl = menuItem.id;
+      }
+      return isValid;
+    })?.children.find(
+      (child) => {
+        const isValid = child.label === menu;
+        if (isValid) {
+          childUrl = child.route;
+        }
+        return isValid;
+      },
+    )?.route;
+
+    void this.router.navigate([`/${parentUrl}/${childUrl}`]);
   }
 }
