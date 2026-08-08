dd9e51b04ddd72ff288b07e8efd3f554291816bc
oleg <oleg_09-95@mail.ru>
2026-08-08 18:24:52 +0300

    feature: добавлена 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Welcome } from './features/welcome/pages/welcome/welcome';
 import { MovieList } from './features/movies/pages/movie-list/movie-list';
+import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
   {path: "", component: Welcome},
   {path: ':movieType', children: [
       {path: '', redirectTo: 'popular', pathMatch: 'full'},
       {path: ':category', component: MovieList}
-    ]}
+    ]},
+  {path: '**', component: NotFound}
 ];
